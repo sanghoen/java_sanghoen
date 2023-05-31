d06583ca6bd4bc29382e6243b7d558023a0df999
sanghoen <dkfnfn2020@naver.com>
2023-05-31 15:23:25 +0900

    자바 : 주석수정

--- java/src/day2/scanner/ScannerEx.java
+++ java/src/day2/scanner/ScannerEx.java
@@ -27,7 +27,9 @@ public class ScannerEx {
 		System.out.println(str2);
 		
 		System.out.println("문자를 입력하세요.");
-		char ch = sc.next().charAt(0);//next()를 이용하여 한 단어를 읽어오고 읽어온 단어의 첫글자를 가져온다
+		//next()를 이용하여 한 단어를 읽어오고 
+		//charAt(0)를 이용하여 읽어온 단어의 첫글자를 가져온다
+		char ch = sc.next().charAt(0);
 		System.out.println(ch);
 		
 		sc.close();//close()로 닫지 않아도 크게 문제는 없지만 안쓰면 주의 표시가 생김
