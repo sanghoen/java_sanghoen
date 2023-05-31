a0123eb2826e7cc1e89588e312d0d443ecbd24c4
sanghoen <dkfnfn2020@naver.com>
2023-05-31 23:53:55 +0900

    자바 2일차 숙제

--- java/src/day2/homework/Ex1.java
+++ java/src/day2/homework/Ex1.java
@@ -1,10 +1,18 @@
 package day2.homework;
 
+import java.util.Scanner;
+
 public class Ex1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("두 정수와 산술연산자를 입력하세요.");
+		String str1 = sc.nextLine();
+		System.out.println(str1);
+		
+		sc.close();
 	}
 
 }
--- java/src/day2/homework/Ex2.java
+++ java/src/day2/homework/Ex2.java
@@ -1,10 +1,42 @@
 package day2.homework;
 
+import java.util.Scanner;
+
 public class Ex2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("두 정수를 입력하세요.");
+		
+		String str = "";
+		
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		char Plus = '+';
+		str = "" + num1 + Plus + num2 + "=" + (num1+num2);
+		System.out.println(str);
+		
+		char Subtract = '-';
+		str = "" + num1 + Subtract + num2 + "=" + (num1-num2);
+		System.out.println(str);
+	
+		char multiply = '*';
+		str = "" + num1 + multiply + num2 + "=" + (num1*num2);
+		System.out.println(str);
+		
+		char divide = '/';
+		double num3 = (double) num1/num2;
+		str = "" + num1 + divide + num2 + "=" + (num3);
+		System.out.println(str);
+		
+		char remain = '%';
+		str = "" + num1 + remain + num2 + "=" + (num1%num2);
+		System.out.println(str);
+		
+		sc.close();
 	}
 
 }
