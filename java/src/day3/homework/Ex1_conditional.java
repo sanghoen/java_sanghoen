796db597013838788ee4ce0d985e851b0c7c4c2c
sanghoen <dkfnfn2020@naver.com>
2023-06-01 15:21:09 +0900

    자바 3일차 연습문제 수정

--- java/src/day3/homework/Ex1_conditional.java
+++ java/src/day3/homework/Ex1_conditional.java
@@ -3,8 +3,16 @@ package day3.homework;
 public class Ex1_conditional {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		/*성별(M:남성,W:여성)를 입력받아 여성인지 확인하는 코드를 작성하세요.
+		 *조건 연산자 이용
+		 *input gender : 
+		 *M
+		 *Are you a women? false
+		 *input gender : 
+		 *W
+		 *Are you a women? true
+		 */
 	}
 
 }
---
+++ java/src/day3/homework/Ex2_if.java
@@ -0,0 +1,10 @@
+package day3.homework;
+
+public class Ex2_if {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
