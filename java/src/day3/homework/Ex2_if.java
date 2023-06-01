baa43768066ec5204007e502346eec43c64b17a5
sanghoen <dkfnfn2020@naver.com>
2023-06-01 15:22:03 +0900

    Update Ex2_if.java

--- java/src/day3/homework/Ex2_if.java
+++ java/src/day3/homework/Ex2_if.java
@@ -3,8 +3,17 @@ package day3.homework;
 public class Ex2_if {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		/*성별(M:남성,W:여성)를 입력받아 M이면 남성을, W이면 여성을 출력하는 코드를 작성하세요. 
+		 * if문 이용
+		 * input gender : 
+		 * M
+		 * Are you a women? man
+		 * input gender : 
+		 * W
+		 * Are you a women?
+		 *  woman
+		 */
 	}
 
 }
