e81d97030dad83554c4bf8f3791ee49a44237fd9
sanghoen <dkfnfn2020@naver.com>
2023-06-05 15:03:04 +0900

    자바 : 숙제 수정

--- java/src/day4/homework/NunberEx.java
+++ java/src/day4/homework/NunberEx.java
@@ -1,5 +1,7 @@
 package day4.homework;
 
+import java.util.Scanner;
+
 public class NunberEx {
 
 	public static void main(String[] args) {
@@ -8,12 +10,34 @@ public class NunberEx {
 		 * (package : day4.homework, file : NumberEx)
 		 * */
 		
-		int num = 3;
+		int num;
+		Scanner sc = new Scanner(System.in);
+		//정수 입력
+		System.out.println("input integer number : ");
+		num = sc.nextInt();
+		
+		//정수 num이 짝수이면 num에 2를 나누고,
+		//num에 2를 나눈 값을 num에 저장
+		if(num % 2 == 0) {
+			num = num / 2;
+			//num /= 2;
+		}
+		//아니면 num에 1을 더한 후
+		//num에 1을 더한 값을 num에 저장
+		else {
+			//num = num + 1;
+			num += 1;
+		
+		//num을 출력
+		System.out.println(num);
+		sc.close();
+		
+		/*int num = 3;
 		
 		if (num % 2 == 0) {
 			System.out.println(num/2);
 		} else {
-			System.out.println(num+1);
+			System.out.println(num+1); 내꺼*/
 		}
 	}
 
