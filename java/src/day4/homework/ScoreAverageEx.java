5bf3248408d1f4699afed9b4da19a9a684406600
sanghoen <dkfnfn2020@naver.com>
2023-06-05 10:12:04 +0900

    자바 : 4일차 과제 - 평균 예제 풀이

--- java/src/day4/homework/ScoreAverageEx.java
+++ java/src/day4/homework/ScoreAverageEx.java
@@ -6,19 +6,27 @@ public class ScoreAverageEx {
 
 	public static void main(String[] args) {
 
-		int kor;
-		int eng;
-		int math;
+		/*국어, 영어, 수학 성적을 입력받아 평균을 출력하는 코드를 작성하세요.
+		 * (package : day4.homework, file : ScoreAverageEx)
+		 * 성적은 정수.
+		 * */
+		
+		int kor, eng, math;
+		double avg;
+		
+		//국어 ,영어 ,수학순으로 성적을입력
+		System.out.println("input score (kor, eng, math, ex : 100 90 80) : ");
+		//평균을 계산
+		//평균을 출력
 		
 		Scanner sc = new Scanner (System.in);
-		System.out.println("enter Korean, English, and Mathematics. : ");
 		kor = sc.nextInt();
 		eng = sc.nextInt();
 		math = sc.nextInt();
 		
-		int sum = kor + eng + math;
-		int avg = sum / 3;
-		System.out.println(avg);
+		avg = (kor + eng + math) / 3.0; // = (double) 3;
+		
+		System.out.println("avg : " + avg);
 		
 		
 		
