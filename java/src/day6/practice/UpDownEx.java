b4ad094ba9e3b904f5ac0d49f734697b075ebc77
sanghoen <dkfnfn2020@naver.com>
2023-06-07 15:21:23 +0900

    자바 6일차 업다운 예제 수정

--- java/src/day6/practice/UpDownEx.java
+++ java/src/day6/practice/UpDownEx.java
@@ -22,31 +22,34 @@ public class UpDownEx {
 		do {
 			//랜덤한 수 생성
 			random = (int)(Math.random() * (max - min + 1) + min);
+			// 0 <= r = 1
+			//min <= r(max - min + 1) < (max + 1)
+			
 			/* 내부 반복문 : Up Down 게임 진행하는 반복문
 			 * 반복횟수 : 입력받은 숫자가 랜덤한 숫자와 일치하지 않으면 반복
 			 * 규칙성 : 입력받은 숫자가 정답보다 크면 Down!, 작으면 Up! 맞으면 Good!을 출력
 			 * */
-			System.out.println(random);
-			do {
-				//입력
-				System.out.println("input : ");
-				num = sc.nextInt();
+				System.out.println(random);
+				do {
+						//입력
+						System.out.println("input : ");
+						num = sc.nextInt();
+						
+						//판별한 후 결과 출력
+						if(num > random) {
+							System.out.println("Down");
+						}else if(num < random) {
+							System.out.println("Up");
+						}else {
+							System.out.println("Good");
+					}
+					
+					}while(random != num);
 				
-				//판별한 후 결과 출력
-				if(num > random) {
-					System.out.println("Down");
-				}else if(num < random) {
-					System.out.println("Up");
-				}else {
-					System.out.println("Good");
-				}
-				
-			}while(random != num);
-			
-			//menu를 선택(더 할건지 말건지)
-			System.out.println("continue?(y/n) : ");
-			menu = sc.next().charAt(0);
-		}while(menu != 'n');
+				//menu를 선택(더 할건지 말건지)
+				System.out.println("continue?(y/n) : ");
+				menu = sc.next().charAt(0);
+			}while(menu != 'n');
 		
 		System.out.println("EXIT!");
 		
