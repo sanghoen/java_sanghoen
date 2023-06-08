d8403fc29bd99fe16f8321a70c46e7309af31d24
sanghoen <dkfnfn2020@naver.com>
2023-06-08 14:45:25 +0900

    자바 : 7일차 과제

---
+++ java/src/day7/homework/StarEx5.java
@@ -0,0 +1,21 @@
+package day7.homework;
+
+public class StarEx5 {
+
+	public static void main(String[] args) {
+
+		//practice - StarEx4 참고
+		
+		/*     *
+		 *    ***
+		 *   *****
+		 *  *******
+		 *   *****
+		 *    ***
+		 *     *
+		 */
+		
+		
+	}
+
+}
