abba14fb5155bff6e6cf3b03b7792878e1766608
sanghoen <dkfnfn2020@naver.com>
2023-06-08 14:44:57 +0900

    자바 :  자바 브레이크 예제

---
+++ java/src/day7/breakex/HellowWorldEx.java
@@ -0,0 +1,19 @@
+package day7.breakex;
+
+public class HellowWorldEx {
+
+	public static void main(String[] args) {
+
+		int i = 1;
+		while(true) {
+			System.out.println("Hellow world!");
+
+			if(i==5) {
+				break;
+			}
+			i++;
+		}
+		
+	}
+
+}
