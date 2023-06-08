fb0897144eb4b220c1435fc4180667f2209d8138
sanghoen <dkfnfn2020@naver.com>
2023-06-08 14:43:13 +0900

    자바 : 컨티뉴문 예제

---
+++ java/src/day7/continueex/EvenNumberEx.java
@@ -0,0 +1,29 @@
+package day7.continueex;
+
+public class EvenNumberEx {
+
+	public static void main(String[] args) {
+
+		//continue문을 이용한 10이하 짝수 출력 예제
+		
+		int i;
+		for( i = 1 ; i <= 10 ; i++ ) {
+			//i가 홀수이면 건너뜀
+			if(i % 2 != 0) {
+				continue; //i++(증강식)로 이동
+			}
+			System.out.println(i); 
+		}
+		
+		i = 0;
+		while( ++i <= 10 ) {
+			if( i % 2 != 0) {
+				continue; // 조건식으로 이동
+			}
+			System.out.println(i);
+			i++;
+		}
+		
+	}
+
+}
