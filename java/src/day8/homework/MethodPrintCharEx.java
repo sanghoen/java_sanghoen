308e29e4555809d49a3fdaa93e49e8f2c03d30d6
sanghoen <dkfnfn2020@naver.com>
2023-06-12 10:34:28 +0900

    자바 : 8일차 과제 - 문자 출력 메서드

--- java/src/day8/homework/MethodPrintCharEx.java
+++ java/src/day8/homework/MethodPrintCharEx.java
@@ -8,14 +8,69 @@ public class MethodPrintCharEx {
 		 * 단, 메서드 이용
 		 */
 		
-	 /* *****
+		 /* *****
+		    ***
+		    ?????		
+		    ^^^^^^^  
+		 */
+	
+		printString("*****");
+	
+		printChar('*', 5);
+		System.out.println();
+		printChar('*', 5, '\n');
+		printChar('=', 6, '\n');
+		/*      *
+		 *     **
+		 *    ***
+		 *   ****
+		 *  *****
+		 * */
+		int num = 5;
+		for(int i = 1; i <= num; i++); {
+			//공백이(' ')이 num - i개 출력
+			printChar(' ', num - i);
+			//'*'이 i개 출력 후 엔터
+			printChar('*', i, '\n');
+			
+		}
 		
-		***
-		
-		?????
-				
-		^^^^^^^  
-	 */
 	}
-
+	/** 문자열이 주어지면 주어진 문자열을 출력하는 줄바꿈 하는 메서드
+	 *  매개변수 : 문자열 => String str
+	 *  리턴타입 : 없음 => void
+	 *  메서드명 : printString
+	 * */
+	public static void printString(String str) {
+		System.out.println(str);
+	}
+	
+	/** 문자와 개수가 주어지면 주어진 문자를 개수만큼 출력하는 메서드
+	 *  매개변수 : 문자와 개수 => char ch, int count
+	 *  리턴타입 : 없음 => void 
+	 *  메서드명 : printChar
+	 * */
+	public static void printChar(char ch, int count) {
+		for(int i = 1; i <= count; i++) {
+			System.out.print(ch);
+		}
+	}
+	/** 문자와 개수, 마지막 문자가 주어지면 주어진 문자를 개수만큼 출력하고 마지막 문자를 출력하는 메서드
+	 *  매개변수 : 문자와 개수, 마지막 문자 => char ch, int count, char lastCh
+	 *  리턴타입 : 출력된 문자열 => String
+	 *  메서드명 : printChar
+	 *  메서드 오버로딩으로 printChar 메서드 추가
+	 * */
+	
+	public static String printChar(char ch, int count, char lastCh) {
+		String str = "";
+		for(int i = 1; i <= count; i++) {
+			System.out.println(ch);
+			str += ch;
+		}
+		System.out.println(lastCh);
+		str += lastCh;
+		return str;
+	}
+	
 }
