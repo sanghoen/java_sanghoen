bd2a942e2b990da01516578de2c1aa8b85c7c085
sanghoen <dkfnfn2020@naver.com>
2023-06-16 15:09:05 +0900

    자바 : 단어장 예제

--- java/src/day12/practice/VocabularyNoteEx2.java
+++ java/src/day12/practice/VocabularyNoteEx2.java
@@ -3,18 +3,44 @@ package day12.practice;
 public class VocabularyNoteEx2 {
 
 	public static void main(String[] args) {
-
-		//영어 단어장 클래스를 만들고 테스크 하는 코드를 작성하세요.
-		//VocabularyEx의 Word 클래스 이용.
-		
 		Word wordList[] = {
 				new Word("abc"),
 				new Word("def","def is define")
 		};
-
+		
 		VocabularyNote note = new VocabularyNote(wordList);
+		//note.print();
+		
+		Word word = new Word("a", "a");
+		note.insert(word);
+		//note.print();
+		note.insert("b","b is b");
+		//note.print();
+		
+		//note.delete("c");//No words found 출력
+		note.delete("a");
+		//note.print();
+		
+		//note.search("a");
+		//note.search("b");
+		
+		note.insert("b", "b");
+		//note.print();
+		
+		note.updateMeaning("b", 2, "B");
+		note.updateMeaning("b", 3, "B");//Wrong number
+		//note.print();
+		
+		note.updateTitle("b", "B");
+		 //No words found 출력. 이미 앞에서 수정해서 b가 없음
+		note.updateTitle("b", "B");
 		note.print();
 	}
 
 }
-		
+
+
+
+
+
+
