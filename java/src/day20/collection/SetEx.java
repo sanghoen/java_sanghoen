7af4afb4973dec3257006919d42767c128bee8f1
sanghoen <dkfnfn2020@naver.com>
2023-06-27 15:00:02 +0900

    자바 : Set 예제

---
+++ java/src/day20/collection/SetEx.java
@@ -0,0 +1,17 @@
+package day20.collection;
+
+import java.util.HashSet;
+
+public class SetEx {
+
+	public static void main(String[] args) {
+
+		HashSet<Integer> set = new HashSet<>();
+		set.add(10);
+		set.add(20);
+		set.add(30);
+		System.out.println(set);
+		
+	}
+
+}
