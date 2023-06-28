ff1993a681a8db91b9b8b882b619ada4570a4104
sanghoen <dkfnfn2020@naver.com>
2023-06-28 15:14:16 +0900

    연습

---
+++ java/src/temporary/controller/GameManager.java
@@ -0,0 +1,10 @@
+package temporary.controller;
+
+public class GameManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java/src/temporary/controller/Program.java
@@ -0,0 +1,5 @@
+package temporary.controller;
+
+public class Program {
+
+}
