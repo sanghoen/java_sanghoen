f312e3017af9fd40e0e42f805904d07926fec009
sanghoen <dkfnfn2020@naver.com>
2023-06-30 15:14:03 +0900

    삭제

--- java/src/day22/practice/controller/Program.java
+++
@@ -1,7 +0,0 @@
-package day22.practice.controller;
-
-public interface Program {
-	void printMenu();
-	void run();
-	void runMenu(int num);
-}
--- java/src/temporary/controller/Program.java
+++
@@ -1,8 +0,0 @@
-package temporary.controller;
-
-public interface Program {
-			void printMenu();
-			void run();
-			void runMenu(int menu);
-	}
-
