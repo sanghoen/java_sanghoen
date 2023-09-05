fcfaa98bb254d39d9056b970e517618be9d98c0f
sanghoen <dkfnfn2020@naver.com>
2023-09-05 12:16:42 +0900

    SPRING 복습 : 게시글 등록, 조회, 삭제 - 첨부파일 적용

--- spring_project/study/src/main/java/kr/kh/study/controller/BoardController.java
+++ spring_project/study/src/main/java/kr/kh/study/controller/BoardController.java
@@ -9,22 +9,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import kr.kh.study.service.BoardService;
 import kr.kh.study.vo.BoardVO;
+import kr.kh.study.vo.FileVO;
 import kr.kh.study.vo.MemberVO;
 
 @Controller
 public class BoardController {
-	
+
 	@Autowired
 	private BoardService boardService;
 	
 	@GetMapping("/board/list")
 	public String boardList(Model model) {
-		//서비스에게 게시글 리스트를 가져오라고 시킴
+		//서비스에게 게시글 리스트를 가져오라고 시킴 
 		List<BoardVO> list = boardService.getBoardList();
-		//가져온 리스트를 화면에 전송
+		//가져온 리스트를 화면에 전송 
 		model.addAttribute("list", list);
 		return "/board/list";
 	}
@@ -34,8 +36,11 @@ public class BoardController {
 		boardService.updateViews(bo_num);
 		//서비스에게 게시글 번호를 주면서 게시글을 가져오라고 요청
 		BoardVO board = boardService.getBoard(bo_num);
+		//게시글을 가져온 뒤, 서비스에게 게시글 번호를 주면서 첨부파일 리스트를 가져오라고 시킴
+		List<FileVO> fileList = boardService.getFileList(bo_num);
 		//가져온 게시글을 화면에 전송
 		model.addAttribute("board", board);
+		model.addAttribute("fileList", fileList);
 		return "/board/detail";
 	}
 	@GetMapping("/board/insert")
@@ -43,10 +48,10 @@ public class BoardController {
 		return "/board/insert";
 	}
 	@PostMapping("/board/insert")
-	public String boardInsertPost(Model model, BoardVO board, HttpSession session) {
+	public String boardInserPost(Model model, BoardVO board, HttpSession session, MultipartFile[] files) {
 		MemberVO user = (MemberVO) session.getAttribute("user");
 		
-		boolean res = boardService.insertBoard(board, user);
+		boolean res = boardService.insertBoard(board, user, files);
 		if(res) {
 			model.addAttribute("msg", "게시글을 등록했습니다.");
 			model.addAttribute("url", "/board/list");
@@ -58,7 +63,7 @@ public class BoardController {
 		return "/util/message";
 	}
 	@GetMapping("/board/update")
-	public String boardUpdate(Model model, Integer bo_num) {
+	public String boardUpdate(Model model,Integer bo_num) {
 		BoardVO board = boardService.getBoard(bo_num);
 		model.addAttribute("board", board);
 		return "/board/update";
@@ -69,37 +74,23 @@ public class BoardController {
 		boolean res = boardService.update(board,user);
 		if(res) {
 			model.addAttribute("msg", "게시글을 수정했습니다.");
-			
 		}else {
-			model.addAttribute("msg", "게시글을 등록하지 못했습니다.");
+			model.addAttribute("msg", "게시글을 수정하지 못했습니다.");
 		}
 		model.addAttribute("url", "/board/detail?bo_num="+board.getBo_num());
 		return "/util/message";
 	}
+	
 	@GetMapping("/board/delete")
-	public String boardDelete(Model model, Integer bo_num, HttpSession session) {
-		MemberVO user =  (MemberVO)session.getAttribute("user");
+	public String boardDelete(Model model,Integer bo_num, HttpSession session) {
+		MemberVO user = (MemberVO)session.getAttribute("user");
 		boolean res = boardService.deleteBoard(bo_num, user);
 		if(res) {
 			model.addAttribute("msg", "게시글을 삭제했습니다.");
-			
 		}else {
 			model.addAttribute("msg", "게시글을 삭제하지 못했습니다.");
 		}
 		model.addAttribute("url", "/board/list");
-		
 		return "/util/message";
 	}
-	
 }
-
-
-
-
-
-
-
-
-
-
-
