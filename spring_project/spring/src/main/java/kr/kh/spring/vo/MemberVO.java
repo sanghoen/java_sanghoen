ac88f8f1bd49a11489a577095f894880a75ca8e7
sanghoen <dkfnfn2020@naver.com>
2023-09-08 12:42:54 +0900

    SPRING : 게시판 타입 수정 구현 완료

--- spring_project/spring/src/main/java/kr/kh/spring/vo/MemberVO.java
+++ spring_project/spring/src/main/java/kr/kh/spring/vo/MemberVO.java
@@ -6,10 +6,10 @@ import lombok.Data;
 
 @Data
 public class MemberVO {
-
 	private String me_id;
 	private String me_pw;
 	private String me_email;
+	private String me_role;
 	private String me_session_id;
 	private Date me_session_limit;
 	
